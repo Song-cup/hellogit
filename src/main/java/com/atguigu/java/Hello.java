7000b71a5893e7553dbb77cc9a25d53230fbb4e3
songlin <2231541055@qq.com>
2021-04-24 11:46:31 +0800

    主干

---
+++ src/main/java/com/atguigu/java/Hello.java
@@ -0,0 +1,12 @@
+package com.atguigu.java;
+
+/**
+ * @author sgl
+ * @DATE 2021/4/24 -1:16
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("nihaoya");
+        System.out.println("更新1");
+    }
+}
