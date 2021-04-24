22efe248e75617df0dc88e4106a58df26bad4af4
songlin <2231541055@qq.com>
2021-04-24 11:48:53 +0800

    分支

--- src/main/java/com/atguigu/java/Hello.java
+++ src/main/java/com/atguigu/java/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("nihaoya");
         System.out.println("更新1");
+        System.out.println("分支1");
     }
 }
